3b96692cadae8d6185d3ccbfe0416ecf448f2fa4
Patrick vd Pols <patrickvanderpols@live.nl>
2019-12-08 19:55:12 +0100

    git test

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/account', 'AccountController@index')->name('account.index');
+
+/**
+ * Test for commit ..git
+ */
